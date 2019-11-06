09df69d2adc3a85144a464512218c040294503d9
Randy Ofosu <randy_ofosu@hotmail.com>
2019-11-06 10:48:57 +0100

    US-011 Competenties

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,11 @@
 function veranderKleur() {
   var mijnProject = document.getElementById("project2");
-  if (mijnProject) {
-    document.getElementById("project2").style.backgroundColor = "blue";
-  } else if (mijnProject == "blue") {
+  if (mijnProject.style.backgroundColor == "blue") {
     document.getElementById("project2").style.backgroundColor = "red";
+  } else {
+    document.getElementById("project2").style.backgroundColor = "blue";
+  }
+
+  if (mijnProject.style.backgroundColor == "red") {
   }
 }
